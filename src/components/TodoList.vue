5f11db8044f77a6ec7684976f9ffcdbe51c1eba4
Srishti Mishra <srishtipmishra@gmail.com>
2018-09-08 20:16:57 -0400

    added todo list component, home page route

---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Todo List
+    </div>
+</template>
+<script>
+export default {
+  name: 'TodoList',
+  data () {
+    return {
+      todoList: {
+        title: '',
+        status: ''
+      }
+    }
+  }
+}
+</script>
+<style>
+</style>
